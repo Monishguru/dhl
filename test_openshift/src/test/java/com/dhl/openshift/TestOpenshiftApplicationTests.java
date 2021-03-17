6d60fe5778a1fe19a5921838dea808ca845aa4df
monishgurug <monishgurug@localhost>
2021-03-18 00:55:51 +0530

    INITIAL COMMIT TO TEST OPEN SHIFT

---
+++ test_openshift/src/test/java/com/dhl/openshift/TestOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhl.openshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
